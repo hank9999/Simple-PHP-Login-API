4a1b253a69af93da55d1f70cff3b694695e5bb42
hank9999 <1825355690@qq.com>
2020-01-08 13:44:37 +0800

    rewrite install

--- install.php
+++ install.php
@@ -11,45 +11,42 @@ PURPOSE.
 See the Mulan PSL v1 for more details.
 */
 
-require_once('./config.php');
-
-$conn = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']); //Create Connect
-if (mysqli_connect_errno()) {
-	echo "MySQL Error: " . mysqli_connect_error() . "<br>"; //Print Error
+if (file_exists("install.lock")) {
+	die("Don't Install Again!<br>If you want to reinstall, please delete install.lock and MySQL Table");
 } else {
-	if (file_exists("install.lock")) {
-		echo "Don't Install Again!<br>If you want to reinstall, please delete install.lock and MySQL Table";
-	} else {
-		echo "Installing...<br>";
-		if (mysqli_query($conn, "CREATE TABLE `user` (`user_id` int not null primary key auto_increment, `permission` int, `username` varchar(255), `password` varchar(255))")) { //Create Table
-			echo "Table 'user' Created Success<br>";
-			if (mysqli_query($conn, "CREATE TABLE `session` (`user_id` int not null primary key auto_increment, `session_id` varchar(255))")) { //Create Table
-				echo "Table 'session' Created Success<br>";
-				if (mysqli_query($conn, "INSERT INTO `user` VALUES (NULL, 0, 'admin', '123456')")) { //Insert Data
-					echo "Userdata Inserted Success<br>";
-					if (mysqli_query($conn, "INSERT INTO `session` VALUES (NULL, '')")) { //Insert Data
-						echo "Session Data Inserted Success<br>";
-						$lock_file = fopen("install.lock", "w");
-						fclose($lock_file);
-						if (file_exists("install.lock")) {
-							echo "Install Success";
-						} else {
-							echo "Please check if you have permission to create files.<br>Install Error";
-						}
-					} else {
-						echo "Session Data Inserted Error: " . mysqli_error($conn) . "<br>";
-					}
-				} else {
-					echo "Userdata Inserted Error: " . mysqli_error($conn) . "<br>";
-				}
-			} else {
-				echo "Table 'session' Created Error: " . mysqli_error($conn) . "<br>";
-			}
-		} else {
-			echo "Table 'user' Created Error: " . mysqli_error($conn) . "<br>";
-		}
+	require_once('./config.php');
+	$conn = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']); //Create Connect
+	if (mysqli_connect_errno()) {
+		die("MySQL Error: " . mysqli_connect_error() . "<br>"); //Print Error
 	}
+	echo "Installing...<br>";
+	if (mysqli_query($conn, "CREATE TABLE `user` (`user_id` int not null primary key auto_increment, `permission` int, `username` varchar(255), `password` varchar(255))")) { //Create Table
+		echo "Table 'user' Created Success<br>";
+	} else {
+		die("Table 'user' Created Error: " . mysqli_error($conn) . "<br>");
+	}
+	if (mysqli_query($conn, "CREATE TABLE `session` (`user_id` int not null primary key auto_increment, `session_id` varchar(255))")) { //Create Table
+		echo "Table 'session' Created Success<br>";
+	} else {
+		die("Table 'session' Created Error: " . mysqli_error($conn) . "<br>");
+	}
+	if (mysqli_query($conn, "INSERT INTO `user` VALUES (NULL, 0, 'admin', '123456')")) { //Insert Data
+		echo "Userdata Inserted Success<br>";
+	} else {
+		die("Userdata Inserted Error: " . mysqli_error($conn) . "<br>");
+	}
+	if (mysqli_query($conn, "INSERT INTO `session` VALUES (NULL, '')")) { //Insert Data
+		echo "Session Data Inserted Success<br>";
+	} else {
+		die("Session Data Inserted Error: " . mysqli_error($conn) . "<br>");
+	}
+	$lock_file = fopen("install.lock", "w");
+	fclose($lock_file);
+	if (file_exists("install.lock")) {
+		echo "Install Success";
+	} else {
+		die("Please check if you have permission to create files.<br>Install Error");
+	}
+	mysqli_close($conn);
 }
-
-mysqli_close($conn);
 ?>
